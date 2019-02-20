47fa98006fb230b2cc55f1a94be9c020fff7c8ad
vmpr <github@it-dd.net>
2019-02-20 16:33:50 +0100

    added compatibility for xcp-ng hypervisor

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,20 @@
 # install
 class secc_sshd::install {
+  
+  # XCP is not a standard operatingsystem type for puppet
+  # so there is not standard package provider definied
+  # this fixes the problem, until puppet will define a standard package provider for "XCP"
 
-  package { 'openssh':
-    ensure => latest,
+  if $::operatingsystem == 'XCP' {
+    package { 'openssh':
+    # we set it to installed, because xcp-ng will handle the updates of sshd by it self
+    ensure => installed,
+    provider => yum,
+    }
   }
-
+  else {
+    package { 'openssh':
+    ensure => latest,
+    }
+  }
 }
